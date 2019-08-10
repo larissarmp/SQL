a56e5815a7617cf20202f53e7061a6180356ba3e
larissarmp <49762354+larissarmp@users.noreply.github.com>
2019-08-09 21:58:04 -0300

    modificação no nome da tabela produto

--- criarTabela.sql
+++ criarTabela.sql
@@ -4,15 +4,15 @@ Data: 09/08/2019
 
 Criar uma tabela para inserir os dados do produto da empresa
 
-Primeiro cria-se as tabelas e por último inserir as regras(chaves primárias, chaves
-secundárias, CONSTRAINS
+Primeiro cria-se as tabelas e por Ãºltimo inserir as regras(chaves primÃ¡rias, chaves
+secundÃ¡rias, CONSTRAINS
 */
 ----------------------------------------------------------------------------------
 
-CREATE TABLE  tb_DadosProdutos(
-	codProduto INT PRIMARY KEY IDENTITY(1,1), --começa com 1 e aumenta de 1 em 1 
-	nmProduto VARCHAR(50) NOT NULL, --sabe-se que essa variável é obrigatória
-	vlProduto DECIMAL(6,2) --valores decimais em que a 1ª parte é a qtd de nº e a 2ª parte é a qtd de vírgula. 
+CREATE TABLE  tb_Produtos(
+	codProduto INT PRIMARY KEY IDENTITY(1,1), --comeÃ§a com 1 e aumenta de 1 em 1 
+	nmProduto VARCHAR(50) NOT NULL, --sabe-se que essa variÃ¡vel Ã© obrigatÃ³ria
+	vlProduto DECIMAL(6,2) --valores decimais em que a 1Âª parte Ã© a qtd de nÂº e a 2Âª parte Ã© a qtd de vÃ­rgula. 
 );
 
 -----------------------------------------------------------------------------------
@@ -37,17 +37,17 @@ CREATE TABLE tb_clientes(
 
 CREATE TABLE  tb_vendas(
 	codVenda INT PRIMARY KEY IDENTITY(1,1),
-	codProduto INT NOT NULL, --quando relacionar duas tabelas utilizar com mesmo nome para facilitar na manutenção 
+	codProduto INT NOT NULL, --quando relacionar duas tabelas utilizar com mesmo nome para facilitar na manutenÃ§Ã£o 
 	vlProduto DECIMAL (6,2) NOT NULL,
 );
 
 -----------------------------------------------------------------------------------
---Criar uma restrição/regra para que a venda seja feita somente se houver produto
+--Criar uma restriÃ§Ã£o/regra para que a venda seja feita somente se houver produto
 --CONSTRAINS
 --FOREIGN KEY
 -----------------------------------------------------------------------------------
 
 ALTER TABLE tb_vendas
-ADD CONSTRAINT FK_Produto_Vendas --para nomear o nome utiliza-se a chave que está saindo e a coluna que está 
-FOREIGN KEY (codProduto) --qual vai ser a coluna que estará referenciando a outra tabela
-REFERENCES tb_DadosProdutos (codProduto)--onde esta fazendo referência 1º -> tabela 2º -> coluna
+ADD CONSTRAINT FK_Produto_Vendas --para nomear o nome utiliza-se a chave que estÃ¡ saindo e a coluna que estÃ¡ 
+FOREIGN KEY (codProduto) --qual vai ser a coluna que estarÃ¡ referenciando a outra tabela
+REFERENCES tb_Produtos (codProduto)--onde esta fazendo referÃªncia 1Âº -> tabela 2Âº -> coluna
