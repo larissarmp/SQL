114e50c49e5c2896329fa0541ae586587f554d3f
larissarmp <49762354+larissarmp@users.noreply.github.com>
2019-08-18 09:55:51 -0300

    Update migrar.sql

--- migrar.sql
+++ migrar.sql
@@ -9,6 +9,9 @@ CREATE TABLE #tmp_Cidades(
 	codEstado INT
 	);
 
+/*IF OBJECT_ID('tb_Estados... #step_Estados') IS NOT NULL
+DROP TABLE #tmp_Estados;*/
+
 -- Inserir os dados das cidades na tabela temporária
 INSERT INTO  #tmp_Cidades(nmCidade)
 SELECT DISTINCT nmCidade FROM tb_clientes ORDER BY nmCidade;
@@ -16,8 +19,18 @@ SELECT DISTINCT nmCidade FROM tb_clientes ORDER BY nmCidade;
 -- Tratamento de dados
 
 --Update do código do estado
-SELECT * FROM tb_Estados E
+--UPDATE MAIS COMPLICADADO
+UPDATE TC SET codEstado = E.codEstado
+FROM #tmp_Cidades TC
+INNER JOIN tb_Clientes T ON TC.nmCidade = T.nmCidade
+INNER JOIN tb_Estados E ON T.nmEstado = E.nmEstado;
+
+SELECT * FROM #tmp_Cidades
+
+SELECT DISTINCT E.codEstado, C.nmCidade FROM tb_Estados E
 INNER JOIN tb_clientes C ON E.nmEstado = C.nmEstado
+
+SELECT * FROM #tmp_Cidades
 
 ROLLBACK TRAN;
 --COMMIT TRAN;
